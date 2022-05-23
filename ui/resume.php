6281f1a46e5760b18f453d1a0ff28a7a79e04687
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2022-05-23 13:16:00 +0530

    commit

--- ui/resume.php
+++ ui/resume.php
@@ -4,7 +4,7 @@ if(isset($_POST['upload'])) {
     $file_type = $_FILES['file']['type'];
     $file_size = $_FILES['file']['size'];
     $file_tem_Loc = $_FILES['file']['tmp_name'];
-    $file_store = "C:/wamp/www/employeeRecruitmentSystem/app/uploadedResume/".$file_name;
+    $file_store = "C:/xampp/htdocs/employeeRecruitmentSystem/app/uploadedResume/".$file_name;
 
     if (move_uploaded_file($file_tem_Loc, $file_store)) {
         echo "File uploaded successfully";
